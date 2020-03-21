b47f6cda4833b5b7282333df93d6bdb14e2a9d33
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-03-21 21:12:12 +0100

    ajoute les colonnes des tables posts & contacts

--- database/migrations/2020_02_06_164514_create_posts_table.php
+++ database/migrations/2020_02_06_164514_create_posts_table.php
@@ -15,6 +15,14 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->timestamp('post_date');
+            $table->text('post_content');
+            $table->text('post_title');
+            $table->string('post_status', 20);
+            $table->string('post_name', 200);
+            $table->string('post_type', 20);
+            $table->text('post_category');
             $table->timestamps();
         });
     }
