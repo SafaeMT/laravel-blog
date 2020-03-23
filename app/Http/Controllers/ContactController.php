07fba7251f1af7ea9ecca59179456a86ef3f30f7
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-03-24 00:42:46 +0100

    enregistre les données du contact & renvoie un message de confirmation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 class ContactController extends Controller
 {
@@ -18,5 +19,13 @@ class ContactController extends Controller
             'mail' => ['required', 'email', 'max:255'],
             'message' => ['min:100']
         ]);
+
+        $contact = new Contact;
+        $contact->contact_name = $request->name;
+        $contact->contact_email = $request->mail;
+        $contact->contact_message = $request->message;
+        $contact->save();
+
+        return view('confirm');
     }
 }
