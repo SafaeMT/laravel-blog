1f7b73105e2c74016369035b15b26375b596a5a4
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-04-11 22:53:03 +0200

    affiche tous les posts dans la page Articles

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,11 +8,25 @@ use App\User;
 
 class PostsController extends Controller
 {
+    /**
+     * Display a listing of the posts sorted by newest.
+     *
+     * @return \Illuminate\Http\Response
+     */
     function index()
     {
-        return view('articles');
+        $posts = Post::all()->sort(function ($a, $b) {
+            return ($a->post_date > $b->post_date) ? -1 : 1;
+        });
+        return view('articles', ['posts' => $posts]);
     }
 
+    /**
+     * Display the specified post.
+     *
+     * @param  string  $post_name
+     * @return \Illuminate\Http\Response
+     */
     public function show($post_name)
     {
         // Get first post with post_name == $post_name and its author's name
