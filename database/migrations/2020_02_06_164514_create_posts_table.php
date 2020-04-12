e37148d2a96c90ce0bb663ced52dd19717efe93c
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-04-12 22:02:29 +0200

    modifie certaines colonnes de la table posts

--- database/migrations/2020_02_06_164514_create_posts_table.php
+++ database/migrations/2020_02_06_164514_create_posts_table.php
@@ -22,12 +22,12 @@ class CreatePostsTable extends Migration
                 // To ensure referential integrity
                 ->onDelete('restrict')
                 ->onUpdate('cascade');
-            $table->timestamp('post_date');
+            $table->timestamp('post_date')->useCurrent();
             $table->text('post_content');
             $table->text('post_title');
-            $table->string('post_status', 20);
+            $table->string('post_status', 20)->nullable();
             $table->string('post_name', 200);
-            $table->string('post_type', 20);
+            $table->string('post_type', 20)->nullable();
             $table->text('post_category');
             $table->timestamps();
         });
